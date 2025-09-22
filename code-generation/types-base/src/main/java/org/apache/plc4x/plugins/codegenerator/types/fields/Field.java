46bf7937875e683b00ffb06a717d6d806bc6c35b
Christofer Dutz <christofer.dutz@c-ware.de>
2025-09-22 10:23:14 +0200

    feat: Added an option to be able to see which attributes are actively set on the current level (and hereby which ones are inherited)

--- code-generation/types-base/src/main/java/org/apache/plc4x/plugins/codegenerator/types/fields/Field.java
+++ code-generation/types-base/src/main/java/org/apache/plc4x/plugins/codegenerator/types/fields/Field.java
@@ -23,12 +23,17 @@ import org.apache.plc4x.plugins.codegenerator.types.definitions.TypeDefinition;
 import org.apache.plc4x.plugins.codegenerator.types.terms.Term;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface Field extends FieldConversions {
 
     TypeDefinition getOwner();
 
     String getTypeName();
+
+    Set<String> getAllAttributeNames();
+
+    Set<String> getCurrentAttributeNames();
 
     Optional<Term> getAttribute(String attributeName);
 
